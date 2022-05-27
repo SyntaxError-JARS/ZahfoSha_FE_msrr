7ab27f307040bc50767c28cffaf7062655cb0334
Romane Orlando Robb <mail@romanerobb.com>
2022-05-26 20:34:34 -0400

    Added more components to the front-end development.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
 <h1>ZahFoSha</h1>
 <h2>by Matthew Swaney and Romane Robb</h2>
 <p>This is the test page for our restaurant.</p>
-<p>We sell pizza.</p>
+<p>We sell pizza and more.</p>
+<button>Place order</button>
+<button>Email us</button>
+<button>Register</button>
+<button>Order From Us</button>
    </div>
   );
 }
